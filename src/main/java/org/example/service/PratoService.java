92e593746c8d19546f3cc40647ff633e55fd08bf
Rafael <rafaelteicas@gmail.com>
2024-02-06 20:53:47 -0300

    create PratoService

---
+++ src/main/java/org/example/service/PratoService.java
@@ -0,0 +1,29 @@
+package org.example.service;
+
+import org.example.entity.Prato;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class PratoService {
+    public static void main(String[] args) {
+        Prato risoto = new Prato();
+        risoto.setNome("Risoto de frutos do mar");
+        risoto.setDescricao("Risoto acompanhado de lula, polvo e mariscos.");
+        risoto.setDisponivel(true);
+        risoto.setValor(BigDecimal.valueOf(88.50));
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("rasFood");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(risoto);
+
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+    }
+}
