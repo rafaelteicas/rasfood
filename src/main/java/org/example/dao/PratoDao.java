4ad03eda96413cc11cbd7e37cb93dd23a4db4b61
Rafael <rafaelteicas@gmail.com>
2024-02-06 21:34:27 -0300

    insert CRUD methods on PratoDao

--- src/main/java/org/example/dao/PratoDao.java
+++ src/main/java/org/example/dao/PratoDao.java
@@ -11,8 +11,20 @@ public class PratoDao {
         this.entityManager = entityManager;
     }
 
-    public void cadastrar(Prato prato) {
+    public void cadastrar(final Prato prato) {
         this.entityManager.persist(prato);
         System.out.println("Entidade cadastrada: " + prato);
+    }
+
+    public Prato consultar(final Integer id) {
+        return this.entityManager.find(Prato.class, id);
+    }
+
+    public void atualizar(final Prato prato) {
+        this.entityManager.merge(prato);
+    }
+
+    public void excluir(final Prato prato) {
+        this.entityManager.remove(prato);
     }
 }
